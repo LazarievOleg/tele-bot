f40f554e205db945d25c1f6bca1584396d161034
Yxod <svryxod@gmail.com>
2020-01-06 15:14:09 +0200

    lesson 15-27

--- index.js
+++ index.js
@@ -1,58 +1,215 @@
 const fetch = require('node-fetch')
 const telegramBot = require('node-telegram-bot-api')
 const help = require('./helpers')
+const fs = require('fs')
 const token = '912579752:AAGR6udQQ7rWOKgXmo9ru7hykrlpvYsXwfw'
 
-const bot = new telegramBot(token, { polling: true })
+const bot = new telegramBot(token, {
+  polling: true
+})
+
+
+//создание и оплата товара 
+
+bot.onText(/\pay/, msg => {
+  const chat_id = msg.chat.id
+
+  bot.sendInvoice(
+    chat_id,
+    'Прем подписка',
+    'Доп функции для телеграм бота',
+    'payload',
+    '632593626:TEST:i56982357197',
+    'Some_string_key',
+    'UAH',
+    [{
+      label: 'Про версия',
+      amount: 20000
+    }],
+    {
+      photo_url: 'https://www.kino-teatr.ru/acter/album/56287/949964.jpg',
+      need_name: true,
+      need_phone_number: true,
+      need_mail: true,
+      is_flexible: true
+    }
+  )
+
+})
 
 
 
+// 
+
+
+
+
+// отправить контакт 
+
+bot.onText(/\/con/, msg => {
+  bot.sendContact(msg.chat.id, '12345678', 'DevCoffee', {
+    last_name: 'Фамилия'
+  })
+})
+
+
+// гео локация 
+bot.onText(/\/loc/, msg => {
+  bot.sendLocation(msg.chat.id, 59.928374, 30.123456)
+})
+
+//отправка видео 
+bot.onText(/\/video/, msg => {
+  const chatID = msg.chat.id
+
+  bot.sendMessage(chatID, 'Гружу видяшку')
+  bot.sendVideo(chatID, './data/videoplayback.mp4')
+})
+
+//отправка видео 
+bot.onText(/\/video/, msg => {
+  const chatID = msg.chat.id
+  bot.sendMessage(chatID, 'Гружу видяшку')
+  fs.readFile('./data/videoplayback.mp4', (err, video) => {
+    bot.sendVideo(chatID.video)
+  })
+
+})
+
+
+// отправка стикеров
+bot.onText(/\/sticker/, msg => {
+  bot.sendSticker(msg.chat.id, './data/1.webp')
+})
+
+bot.onText(/\/sticker/, msg => {
+  fs.readFile('./data/1.webp', (err, sticker) => {
+    bot.sendSticker(msg.chat.id, sticker)
+  })
+})
+
+
+
+// отправляем файл
+
+bot.onText(/\/doc1/, msg => {
+  bot.sendDocument(msg.chat.id, './data/sheets/test.xlsx')
+})
+
+
+
+// отправляем файл со статусами 
+bot.onText(/\/doc2/, msg => {
+  bot.sendMessage(msg.chat.id, 'Загрузка начата')
+
+  fs.readFile('./data/new.zip', (err, file) => {
+    bot.sendDocument(msg.chat.id, file, {
+      caption: 'Some text'
+    }).then(() => {
+      bot.sendMessage(msg.chat.id, 'Загрузили наконец-то')
+    })
+  })
+})
+
+
+
+// отправка картинки
+bot.onText(/\/pic/, msg => {
+  bot.sendPhoto(msg.chat.id, fs.readFileSync(__dirname + '/data/img/img.jpg'))
+})
+
+// второй вариант отправки картинки с заголовком
+bot.onText(/\/pic2/, msg => {
+  bot.sendPhoto(msg.chat.id, './data/img/img.jpg', {
+    caption: 'Пример заголовка для картинки'
+  })
+})
+
+// отправка аудио записи
+bot.onText(/\/audio/, msg => {
+  bot.sendAudio(msg.chat.id, './data/audio/example.mp3')
+})
+
+
+// отправка аудио записи с подписями о старте и завершении
+bot.onText(/\/audio2/, msg => {
+  bot.sendMessage(msg.chat.id, "Гружу твой файл епт")
+  fs.readFile(__dirname + '/data/audio/example.mp3', (err, data) => {
+    bot.sendAudio(msg.chat.id, data).then(() => {
+      bot.sendMessage(msg.chat.id, "Загрузка завершил")
+    })
+  })
+})
+
+
+
+// inline клава
 const inline_keyboard = [
   [{
-text: 'forward',
-callback_data: 'forward' 
-  },
-  {
-text: 'reply',
-callback_data: 'reply' 
-  }],
+      text: 'forward',
+      callback_data: 'forward'
+    },
+    {
+      text: 'reply',
+      callback_data: 'reply'
+    }
+  ],
   [{
-text: 'edit',
-callback_data: 'edit' 
-  },
-  {
-text: 'delete',
-callback_data: 'delete' 
-  }]
+      text: 'edit',
+      callback_data: 'edit'
+    },
+    {
+      text: 'delete',
+      callback_data: 'delete'
+    },
+    {
+      text: 'get img',
+      callback_data: 'get img'
+    }
+  ]
 ]
 
 bot.on('callback_query', query => {
 
-const {chat, message_id, text} = query.message
+  const {
+    chat,
+    message_id,
+    text
+  } = query.message
 
   switch (query.data) {
     case 'forward':
       bot.forwardMessage(chat.id, chat.id, message_id)
       break
     case 'reply':
-        bot.sendMessage(chat.id, 'Отправим сообщение' , {
-            raply_to_messange_id: message_id
-        })
-        break
+      bot.sendMessage(chat.id, 'Отправим сообщение', {
+        reply_to_message_id: message_id
+      })
+      break
 
     case 'edit':
-   
-      bot.editMessageText( `${text} (edited)`, {
-            chat_id: chat.id,
-            message_id: message_id
-        })
-        break
-        
+      bot.editMessageText(`${text} (edited)`, {
+        chat_id: chat.id,
+        message_id: message_id,
+        reply_markup: {
+          inline_keyboard
+        }
+      })
+      break
+
+      // удаляем месаг 
+    case 'delete':
+      bot.deleteMessage(chat.id, message_id)
+      break
+    case 'get img':
+      bot.sendPhoto(chat.id, fs.readFileSync(__dirname + '/img.jpg'))
+      break
+
   }
 
-bot.answerCallbackQuery({
-  callback_query_id: query.id
-})
+  bot.answerCallbackQuery({
+    callback_query_id: query.id
+  })
 })
 
 bot.onText(/\/start/, (msg, [source, match]) => {
@@ -211,20 +368,16 @@ bot.onText(/\/start/, (msg, [source, match]) => {
 //     console.error(error);
 // })
 
-bot.onText(/\/start/, msg => {
-  const { id } = msg.chat;
-  bot.sendMessage(id, help.debug(msg))
-})
-
-
-bot.onText(/\/test (.+)/, (msg, [source, match]) => {
-  const { id } = msg.chat;
-  bot.sendMessage(id, help.debug(match))
-})
 
 
 
+// bot.onText(/\/start/, msg => {
+//   const { id } = msg.chat;
+//   bot.sendMessage(id, help.debug(msg))
+// })
 
 
-
-
+// bot.onText(/\/test (.+)/, (msg, [source, match]) => {
+//   const { id } = msg.chat;
+//   bot.sendMessage(id, help.debug(match))
+// })
