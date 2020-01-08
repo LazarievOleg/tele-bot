b6aae364a1a45cdfb0de4fcf81ee872d32549744
captify-lazariev <oleg.lazariev@captify.co.uk>
2020-01-09 00:14:32 +0200

    commands added

--- db-helper/db-helper.js
+++ db-helper/db-helper.js
@@ -1,12 +1,14 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  user: 'rw.tests.db.qa',
-  host: process.env.AP_PSQL_PORT_5432_TCP_ADDR || 'localhost',
-  database: 'c3',
-  password: 'rds_for_the_win',
-  port: process.env.AP_PSQL_PORT_5432_TCP_PORT || 6583,
+  user: 'docker',
+  host: process.env.PSQL_PORT_5432_TCP_ADDR || 'localhost',
+  database: 'docker',
+  password: 'docker',
+  port: process.env.PSQL_PORT_5432_TCP_PORT || 6588,
 });
+
+client.connect();
 
 module.exports = {
   connectDB() {
@@ -17,22 +19,20 @@ module.exports = {
     client.end();
   },
 
-  select(condition, column = '*', table = 'conversion_pixel') {
+  async insertTeleBot(){
+    await client.query(`INSERT INTO tele_bot (chat_id, web_sites) VALUES (11, 'website')  RETURNING *`);
+  },
+
+
+   async selectUrls(condition, column = '*', table = 'tele_bot') {
     return client.query(`SELECT ${column} FROM  ${table} WHERE  ${condition}`);
   },
 
-  insertEmptyAdvertiser() {
-    return client.query(`INSERT INTO advertiser (name) VALUES ('empty test advertiser${Date.now()}') RETURNING *`);
+  async insertUrl(chatId, url) {
+    return client.query(`INSERT INTO tele_bot (chat_id, web_sites) VALUES (${chatId}, 'http://${url}') RETURNING *`);
   },
 
-  deleteByPixelID(pixelID) {
-    return client.query(`DELETE FROM conversion_pixel WHERE id = ${pixelID}`);
-  },
-
-  createPixelWithAppnexusIDs(pixelName, url, appnexusSegmentId, appnexusConversionId) {
-    return client.query(`INSERT INTO conversion_pixel
-    (pixel_name, appnexus_name, url, is_revenue, is_order_id, is_pixel_hub, advertiser, version, is_new, is_deleted, category_id, appnexus_advertiser_id, appnexus_segment_id, appnexus_conversion_id)
-    VALUES
-    ('${pixelName}', 'appnexus name', '${url}', 'true', 'true', 'false', 1 , 1, false, false, 1, 12345, ${appnexusSegmentId}, ${appnexusConversionId}) RETURNING *`);
+  deleteUrl(url) {
+    return client.query(`DELETE FROM tele_bot WHERE web_sites LIKE '%${url}'`);
   },
 };
--- index.js
+++ index.js
@@ -2,224 +2,290 @@ const fetch = require('node-fetch')
 const telegramBot = require('node-telegram-bot-api')
 const help = require('./helpers')
 const fs = require('fs')
-const token = '912579752:AAGR6udQQ7rWOKgXmo9ru7hykrlpvYsXwfw'
+const token = '1048847285:AAF-i8fWvbMqpZOTPYld8-7Cuyuy8QOBNaQ'
+const db = require('./db-helper/db-helper.js')
 
 const bot = new telegramBot(token, {
   polling: true
 })
 
 
-//создание и оплата товара 
+let interval = null;
 
-bot.onText(/\pay/, msg => {
-  const chat_id = msg.chat.id
+bot.onText(/\/get/, (msg) => {
 
-  bot.sendInvoice(
-    chat_id,
-    'Прем подписка',
-    'Доп функции для телеграм бота',
-    'payload',
-    '632593626:TEST:i56982357197',
-    'Some_string_key',
-    'UAH',
-    [{
-      label: 'Про версия',
-      amount: 20000
-    }],
-    {
-      photo_url: 'https://www.kino-teatr.ru/acter/album/56287/949964.jpg',
-      need_name: true,
-      need_phone_number: true,
-      need_mail: true,
-      is_flexible: true
-    }
-  )
+  let urls = [];
+  const { id } = msg.chat
 
+  db.selectUrls(`chat_id = ${id}`).then(DBResponse => {
+    DBResponse.rows.map(each => urls.push(each.web_sites))
+  });
+  interval = setInterval(() => {
+    urls.forEach(web => {
+      fetch(web)
+        .then(response => {
+          console.log(id + ' ' + web + ' ' + response.status + ' ' + response.statusText);
+          bot.sendMessage(id, `status code of ${web} is ${response.status} ${response.statusText} `, {
+            disable_web_page_preview: true
+          });
+        }).catch(error => {
+          console.log(id + ' ' + web + ' ' + error.message)
+          bot.sendMessage(id, help.debug(error.message) + '   !!! CHECK YOUR URL!!!')
+        })
+    });
+  }, 15000);
+});
+
+bot.onText(/\/start/, (msg) => {
+  const { id } = msg.chat
+  bot.sendMessage(id, `
+  /get - start getting status codes
+  /stop - stop it now!
+  /addurl - add URL to your list, example: /addurl snn.com
+  /delurl - remove URL from your list, example: /delurl snn.com
+  /myurls - get list with all your URLs`);
+});
+
+bot.onText(/\/stop/, (msg) => {
+  const { id } = msg.chat
+  setTimeout(() => { clearInterval(interval) }, 100);
+  bot.sendMessage(id, `stoped`);
+});
+
+
+bot.onText(/\/addurl (.+)/, (msg, [source, match]) => {
+  const { id } = msg.chat
+  db.insertUrl(id, match);
+  bot.sendMessage(id, help.debug(match) + ' url added')
 })
 
-
-
-// 
-
-
-
-
-// отправить контакт 
-
-bot.onText(/\/con/, msg => {
-  bot.sendContact(msg.chat.id, '12345678', 'DevCoffee', {
-    last_name: 'Фамилия'
-  })
+bot.onText(/\/delurl (.+)/, (msg, [source, match]) => {
+  const { id } = msg.chat
+  db.deleteUrl(match);
+  bot.sendMessage(id, help.debug(match) + ' url deleted')
 })
 
-
-// гео локация 
-bot.onText(/\/loc/, msg => {
-  bot.sendLocation(msg.chat.id, 59.928374, 30.123456)
-})
-
-//отправка видео 
-bot.onText(/\/video/, msg => {
-  const chatID = msg.chat.id
-
-  bot.sendMessage(chatID, 'Гружу видяшку')
-  bot.sendVideo(chatID, './data/videoplayback.mp4')
-})
-
-//отправка видео 
-bot.onText(/\/video/, msg => {
-  const chatID = msg.chat.id
-  bot.sendMessage(chatID, 'Гружу видяшку')
-  fs.readFile('./data/videoplayback.mp4', (err, video) => {
-    bot.sendVideo(chatID.video)
-  })
-
-})
-
-
-// отправка стикеров
-bot.onText(/\/sticker/, msg => {
-  bot.sendSticker(msg.chat.id, './data/1.webp')
-})
-
-bot.onText(/\/sticker/, msg => {
-  fs.readFile('./data/1.webp', (err, sticker) => {
-    bot.sendSticker(msg.chat.id, sticker)
-  })
-})
-
-
-
-// отправляем файл
-
-bot.onText(/\/doc1/, msg => {
-  bot.sendDocument(msg.chat.id, './data/sheets/test.xlsx')
-})
-
-
-
-// отправляем файл со статусами 
-bot.onText(/\/doc2/, msg => {
-  bot.sendMessage(msg.chat.id, 'Загрузка начата')
-
-  fs.readFile('./data/new.zip', (err, file) => {
-    bot.sendDocument(msg.chat.id, file, {
-      caption: 'Some text'
-    }).then(() => {
-      bot.sendMessage(msg.chat.id, 'Загрузили наконец-то')
-    })
-  })
-})
-
-
-
-// отправка картинки
-bot.onText(/\/pic/, msg => {
-  bot.sendPhoto(msg.chat.id, fs.readFileSync(__dirname + '/data/img/img.jpg'))
-})
-
-// второй вариант отправки картинки с заголовком
-bot.onText(/\/pic2/, msg => {
-  bot.sendPhoto(msg.chat.id, './data/img/img.jpg', {
-    caption: 'Пример заголовка для картинки'
-  })
-})
-
-// отправка аудио записи
-bot.onText(/\/audio/, msg => {
-  bot.sendAudio(msg.chat.id, './data/audio/example.mp3')
-})
-
-
-// отправка аудио записи с подписями о старте и завершении
-bot.onText(/\/audio2/, msg => {
-  bot.sendMessage(msg.chat.id, "Гружу твой файл епт")
-  fs.readFile(__dirname + '/data/audio/example.mp3', (err, data) => {
-    bot.sendAudio(msg.chat.id, data).then(() => {
-      bot.sendMessage(msg.chat.id, "Загрузка завершил")
-    })
-  })
-})
-
-
-
-// inline клава
-const inline_keyboard = [
-  [{
-      text: 'forward',
-      callback_data: 'forward'
-    },
-    {
-      text: 'reply',
-      callback_data: 'reply'
-    }
-  ],
-  [{
-      text: 'edit',
-      callback_data: 'edit'
-    },
-    {
-      text: 'delete',
-      callback_data: 'delete'
-    },
-    {
-      text: 'get img',
-      callback_data: 'get img'
-    }
-  ]
-]
-
-bot.on('callback_query', query => {
-
-  const {
-    chat,
-    message_id,
-    text
-  } = query.message
-
-  switch (query.data) {
-    case 'forward':
-      bot.forwardMessage(chat.id, chat.id, message_id)
-      break
-    case 'reply':
-      bot.sendMessage(chat.id, 'Отправим сообщение', {
-        reply_to_message_id: message_id
+bot.onText(/\/myurls/, (msg) => {
+  const { id } = msg.chat
+  db.selectUrls(`chat_id = ${id}`).then(response => {
+    response.rows.map(each => {
+      bot.sendMessage(id, help.debug(each.web_sites), {
+        disable_web_page_preview: true
       })
-      break
-
-    case 'edit':
-      bot.editMessageText(`${text} (edited)`, {
-        chat_id: chat.id,
-        message_id: message_id,
-        reply_markup: {
-          inline_keyboard
-        }
-      })
-      break
-
-      // удаляем месаг 
-    case 'delete':
-      bot.deleteMessage(chat.id, message_id)
-      break
-    case 'get img':
-      bot.sendPhoto(chat.id, fs.readFileSync(__dirname + '/img.jpg'))
-      break
-
-  }
-
-  bot.answerCallbackQuery({
-    callback_query_id: query.id
+    });
   })
 })
+//создание и оплата товара
 
-bot.onText(/\/start/, (msg, [source, match]) => {
-  const chatID = msg.chat.id
-  bot.sendMessage(chatID, 'keyboard', {
-    reply_markup: {
-      inline_keyboard
-    }
-  })
-})
+// bot.onText(/\pay/, msg => {
+//   const chat_id = msg.chat.id
+
+//   bot.sendInvoice(
+//     chat_id,
+//     'Прем подписка',
+//     'Доп функции для телеграм бота',
+//     'payload',
+//     '632593626:TEST:i56982357197',
+//     'Some_string_key',
+//     'UAH',
+//     [{
+//       label: 'Про версия',
+//       amount: 20000
+//     }],
+//     {
+//       photo_url: 'https://www.kino-teatr.ru/acter/album/56287/949964.jpg',
+//       need_name: true,
+//       need_phone_number: true,
+//       need_mail: true,
+//       is_flexible: true
+//     }
+//   )
+
+// })
+
+
+
+// // 
+
+
+
+
+// // отправить контакт 
+
+// bot.onText(/\/con/, msg => {
+//   bot.sendContact(msg.chat.id, '12345678', 'DevCoffee', {
+//     last_name: 'Фамилия'
+//   })
+// })
+
+
+// // гео локация 
+// bot.onText(/\/loc/, msg => {
+//   bot.sendLocation(msg.chat.id, 59.928374, 30.123456)
+// })
+
+// //отправка видео 
+// bot.onText(/\/video/, msg => {
+//   const chatID = msg.chat.id
+
+//   bot.sendMessage(chatID, 'Гружу видяшку')
+//   bot.sendVideo(chatID, './data/videoplayback.mp4')
+// })
+
+// //отправка видео 
+// bot.onText(/\/video/, msg => {
+//   const chatID = msg.chat.id
+//   bot.sendMessage(chatID, 'Гружу видяшку')
+//   fs.readFile('./data/videoplayback.mp4', (err, video) => {
+//     bot.sendVideo(chatID.video)
+//   })
+
+// })
+
+
+// // отправка стикеров
+// bot.onText(/\/sticker/, msg => {
+//   bot.sendSticker(msg.chat.id, './data/1.webp')
+// })
+
+// bot.onText(/\/sticker/, msg => {
+//   fs.readFile('./data/1.webp', (err, sticker) => {
+//     bot.sendSticker(msg.chat.id, sticker)
+//   })
+// })
+
+
+
+// // отправляем файл
+
+// bot.onText(/\/doc1/, msg => {
+//   bot.sendDocument(msg.chat.id, './data/sheets/test.xlsx')
+// })
+
+
+
+// // отправляем файл со статусами 
+// bot.onText(/\/doc2/, msg => {
+//   bot.sendMessage(msg.chat.id, 'Загрузка начата')
+
+//   fs.readFile('./data/new.zip', (err, file) => {
+//     bot.sendDocument(msg.chat.id, file, {
+//       caption: 'Some text'
+//     }).then(() => {
+//       bot.sendMessage(msg.chat.id, 'Загрузили наконец-то')
+//     })
+//   })
+// })
+
+
+
+// // отправка картинки
+// bot.onText(/\/pic/, msg => {
+//   bot.sendPhoto(msg.chat.id, fs.readFileSync(__dirname + '/data/img/img.jpg'))
+// })
+
+// // второй вариант отправки картинки с заголовком
+// bot.onText(/\/pic2/, msg => {
+//   bot.sendPhoto(msg.chat.id, './data/img/img.jpg', {
+//     caption: 'Пример заголовка для картинки'
+//   })
+// })
+
+// // отправка аудио записи
+// bot.onText(/\/audio/, msg => {
+//   bot.sendAudio(msg.chat.id, './data/audio/example.mp3')
+// })
+
+
+// // отправка аудио записи с подписями о старте и завершении
+// bot.onText(/\/audio2/, msg => {
+//   bot.sendMessage(msg.chat.id, "Гружу твой файл епт")
+//   fs.readFile(__dirname + '/data/audio/example.mp3', (err, data) => {
+//     bot.sendAudio(msg.chat.id, data).then(() => {
+//       bot.sendMessage(msg.chat.id, "Загрузка завершил")
+//     })
+//   })
+// })
+
+
+
+// // inline клава
+// const inline_keyboard = [
+//   [{
+//       text: 'forward',
+//       callback_data: 'forward'
+//     },
+//     {
+//       text: 'reply',
+//       callback_data: 'reply'
+//     }
+//   ],
+//   [{
+//       text: 'edit',
+//       callback_data: 'edit'
+//     },
+//     {
+//       text: 'delete',
+//       callback_data: 'delete'
+//     },
+//     {
+//       text: 'get img',
+//       callback_data: 'get img'
+//     }
+//   ]
+// ]
+
+// bot.on('callback_query', query => {
+
+//   const {
+//     chat,
+//     message_id,
+//     text
+//   } = query.message
+
+//   switch (query.data) {
+//     case 'forward':
+//       bot.forwardMessage(chat.id, chat.id, message_id)
+//       break
+//     case 'reply':
+//       bot.sendMessage(chat.id, 'Отправим сообщение', {
+//         reply_to_message_id: message_id
+//       })
+//       break
+
+//     case 'edit':
+//       bot.editMessageText(`${text} (edited)`, {
+//         chat_id: chat.id,
+//         message_id: message_id,
+//         reply_markup: {
+//           inline_keyboard
+//         }
+//       })
+//       break
+
+//       // удаляем месаг 
+//     case 'delete':
+//       bot.deleteMessage(chat.id, message_id)
+//       break
+//     case 'get img':
+//       bot.sendPhoto(chat.id, fs.readFileSync(__dirname + '/img.jpg'))
+//       break
+
+//   }
+
+//   bot.answerCallbackQuery({
+//     callback_query_id: query.id
+//   })
+// })
+
+// bot.onText(/\/start/, (msg, [source, match]) => {
+//   const chatID = msg.chat.id
+//   bot.sendMessage(chatID, 'keyboard', {
+//     reply_markup: {
+//       inline_keyboard
+//     }
+//   })
+// })
 
 
 
